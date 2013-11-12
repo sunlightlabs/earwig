efdbbd165bf42901b2581d2b4da559e6b4f7e347
Paul Tagliamonte <tag@pault.ag>
2013-11-12 11:36:19 -0500

    Cleanup models

--- contact/plugins/fnord/models.py
+++ contact/plugins/fnord/models.py
@@ -3,5 +3,5 @@ from contact.models import DeliveryAttempt
 
 
 class FnordStatus(models.Model):
-    attempt = models.ForeignKey(DeliveryAttempt, uniqe=True)
-    remote_id = modes.ChatField(max_size=36)
+    attempt = models.ForeignKey(DeliveryAttempt, unique=True)
+    remote_id = models.CharField(max_length=36)
