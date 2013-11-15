cc4a90dce118f2b0fa79016b0544a082cd211d7c
James Turk <james.p.turk@gmail.com>
2013-11-15 15:41:51 -0500

    test msg_to_json

--- contact/tests.py
+++ contact/tests.py
@@ -1,16 +1,37 @@
 import json
 from django.test import TestCase, Client
 from .models import Person, Sender, Message, MessageRecipient
+from .views import _msg_to_json
 
 GOOD_MESSAGE = {'type': 'public', 'subject': 'hi', 'message': 'this is a message',
                 'sender': '1-2-3-4', 'recipients': ['ocd-person/1']}
 
-class TestCreateMessage(TestCase):
+class TestViews(TestCase):
 
     def setUp(self):
-        Person.objects.create(ocd_id='ocd-person/1', title='President', name='Gerald Fnord')
-        Person.objects.create(ocd_id='ocd-person/2', title='Mayor', name='Rob Fnord')
-        Sender.objects.create(id='1-2-3-4')
+        self.person1 = Person.objects.create(ocd_id='ocd-person/1', title='President',
+                                             name='Gerald Fnord')
+        self.person2 = Person.objects.create(ocd_id='ocd-person/2', title='Mayor',
+                                             name='Rob Fnord')
+        self.sender = Sender.objects.create(id='1-2-3-4')
+
+    def test_msg_to_json(self):
+        msg = Message.objects.create(type='private', sender=self.sender, subject='subject',
+                                     message='hello everyone')
+        MessageRecipient.objects.create(message=msg, recipient=self.person1, status='pending')
+        MessageRecipient.objects.create(message=msg, recipient=self.person2, status='expired')
+
+        data = json.loads(_msg_to_json(msg))
+
+        assert data['message'] == 'hello everyone'
+        assert data['type'] == 'private'
+        assert data['sender'] == self.sender.id
+        assert data['subject'] == 'subject'
+        assert len(data['recipients']) == 2
+        assert data['recipients'][0]['status'] == 'pending'
+        assert data['recipients'][0]['recipient_id'] == self.person1.id
+        assert data['recipients'][1]['status'] == 'expired'
+        assert data['recipients'][1]['recipient_id'] == self.person2.id
 
     def test_required_fields(self):
         """ test that type, subject, message, sender are required """
