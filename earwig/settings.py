db68c7c71473e7b465e251549eba7eefb9d2e358
Paul Tagliamonte <tag@pault.ag>
2013-11-19 11:57:04 -0500

    Add templates in.

--- earwig/settings.py
+++ earwig/settings.py
@@ -107,9 +107,7 @@ ROOT_URLCONF = 'earwig.urls'
 WSGI_APPLICATION = 'earwig.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates')),
 )
 
 INSTALLED_APPS = (
