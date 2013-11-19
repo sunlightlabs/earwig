4c33ab174758e7486197e8f597a66e85cae81d88
Paul Tagliamonte <tag@pault.ag>
2013-11-19 17:09:33 -0500

    Start to add unsubscribe endpoints.

--- contact/urls.py
+++ contact/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
+    url(r'^unsubscribe/(?P<secret>.*)/$', 'contact.views.unsubscribe'),
+
     url(r'^message/$', 'contact.views.create_message', name='create_message'),
     url(r'^message/(?P<message_id>\d+)/$', 'contact.views.get_message', name='get_message'),
 )
