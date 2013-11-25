db64b6deb94e087eea4bc3ed77a5c6d4b5e7ce3c
twneale <twneale@gmail.com>
2013-11-25 13:43:51 -0500

    Added function to check contact detail isn't blacklisted and message medium is correct.

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,8 +1,22 @@
-from contact.errors import CantCancelMessage
+from contact.errors import CantCancelMessage, Blacklisted
 
 
 class ContactPlugin(object):
     """ """
+    def check_contact_detail(self, attempt):
+        '''Code might send messages by calling this method.
+        '''
+        contact_detail = attempt.contact
+
+        # Verify the contact detail is not blacklisted.
+        if contact_detail.blacklisted:
+            msg = '%s is blacklisted.'
+            raise Blacklisted(msg % contact_detail.value)
+
+        # Verify the correct contact detail type has been passed.
+        if contact_detail.type != self.medium:
+            msg = "'%s' only supports messages of type %r"
+            raise InvalidContactType(msg % (self.__class__, self.medium))
 
     def send_message(self, attempt):
         """ """
