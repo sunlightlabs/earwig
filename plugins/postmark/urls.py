5b4547e0795d35adae9bd26348c4198ccddf4338
twneale <twneale@gmail.com>
2013-12-05 17:35:50 -0500

    wired in postmark urls and document bounce view

--- plugins/postmark/urls.py
+++ plugins/postmark/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 
-urlpatterns = patterns('contact.plugin.postmark',
+urlpatterns = patterns('plugins.postmark.views',
     # These urls need to match the webhook urls configured through the
     # postmark UI.
     url(r'^bounce/$', 'handle_bounce'),
--- plugins/postmark/views.py
+++ plugins/postmark/views.py
@@ -1,11 +1,27 @@
-from ..contact.models import Message  # etc.
+from django.http import HttpResponse
+
+from contact.models import Message  # etc.
 
 
 def handle_bounce(request):
     '''Body should be a json payload. See:
-    http://developer.postmarkapp.com/developer-inbound-configure.html
+    http://developer.postmarkapp.com/developer-bounces.html#bounce-hooks
+    {
+      "ID" : [ID],
+      "Type" : "HardBounce",
+      "Tag" : "Invitation",
+      "MessageID" : "d12c2f1c-60f3-4258-b163-d17052546ae4",
+      "TypeCode" : 1,
+      "Email" : "jim@test.com",
+      "BouncedAt" : "2010-04-01",
+      "Details" : "test bounce",
+      "DumpAvailable" : true,
+      "Inactive" : true,
+      "CanActivate" : true,
+      "Subject" : "Hello from our app!"
+    }
     '''
-
+    return HttpResponse()
 
 
 def handle_inbound(request):
