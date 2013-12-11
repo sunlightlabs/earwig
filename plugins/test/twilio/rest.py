69ad17edd9376842f894360d36728c60c83270d8
Paul Tagliamonte <tag@pault.ag>
2013-12-11 10:46:06 -0500

    roughly check some args.

--- plugins/test/twilio/rest.py
+++ plugins/test/twilio/rest.py
@@ -2,8 +2,15 @@ from twilio import TwilioRestException
 
 class FakeRestEndpoint(object):
     def create(self, *args, **kwargs):
+        if 'to' not in kwargs:
+            raise TypeError("Need a to number")
+
+        if 'from_' not in kwargs:
+            raise TypeError("Need a from number")
+
         if kwargs.get('to', None) == 'bad':
             raise TwilioRestException("Bad to")
+
         return True
 
 
