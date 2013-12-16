05b8d44d74e95f090f3d1da8d757083e64082147
twneale <twneale@gmail.com>
2013-12-16 16:06:53 -0500

    Added names for plugin urls (to make them `reverse`able)

--- plugins/postmark/urls.py
+++ plugins/postmark/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('plugins.postmark.views',
     # These urls need to match the webhook urls configured through the
     # postmark UI.
-    url(r'^bounce/$', 'handle_bounce'),
-    url(r'^inbound/$', 'handle_inbound'),
+    url(r'^bounce/$', 'handle_bounce', name='handle_bounce'),
+    url(r'^inbound/$', 'handle_inbound', name='handle_inbound'),
 )
