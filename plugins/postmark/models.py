50468fd4bfb8a04f9e43183c6957eb2131c7e196
twneale <twneale@gmail.com>
2013-12-16 16:05:33 -0500

    Trim down postmark meta object, added unicode method.

--- plugins/postmark/models.py
+++ plugins/postmark/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 
 class PostmarkDeliveryMeta(models.Model):
-    attempt = models.ForeignKey('contact.models.DeliveryAttempt', unique=True)
+    attempt = models.ForeignKey('contact.DeliveryAttempt', unique=True)
     message_id = models.CharField(max_length=36)
+
+    def __unicode__(self):
+        return self.message_id
