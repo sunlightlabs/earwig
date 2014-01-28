6a133be7c930eec982e44abbc3cd4a4bc2af95a8
James Turk <james.p.turk@gmail.com>
2014-01-28 15:31:40 -0500

    dropping half-baked sendgrid implementation as they told us they weren't ok with the usage

--- plugins/sendgrid/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from contact.models import DeliveryAttempt
-
-
-class SendgridDeliveryMeta(models.Model):
-    attempt = models.ForeignKey(DeliveryAttempt, unique=True)
-    # request_id = models.CharField(max_length=36)
-    # message_id = models.CharField(max_length=36)
--- plugins/sendgrid/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-
-urlpatterns = patterns('contact.plugin.sendgrid',
-    # See http://sendgrid.com/docs/API_Reference/Webhooks/event.html
-    url(r'^bounce/$', 'handle_bounce'),
-    # http://sendgrid.com/docs/API_Reference/Webhooks/parse.html
-    url(r'^inbound/$', 'handle_inbound'),
-)
