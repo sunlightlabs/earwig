d37bdd721015e916faa3c9ba168a5bff8e8403ea
Paul Tagliamonte <tag@pault.ag>
2014-01-31 11:20:52 -0500

    Fix unsubscribe URL property

--- contact/models.py
+++ contact/models.py
@@ -166,5 +166,6 @@ class DeliveryAttempt(models.Model):
 
     @property
     def unsubscribe_url(self):
-        return ''.join(settings.EARWIG_PUBLIC_LINK_ROOT,
-                       reverse('flag', args=(str(self.id), str(self.unsubscribe_token()))))
+        return ''.join([settings.EARWIG_PUBLIC_LINK_ROOT,
+                       reverse('flag', args=(str(self.id),
+                                             str(self.unsubscribe_token())))])
