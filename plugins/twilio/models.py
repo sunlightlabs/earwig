1fe62020b75754f505c6b6fae6babe26bc2a67be
Paul Tagliamonte <tag@pault.ag>
2014-02-03 11:10:12 -0500

    junk got thrown into a comment

--- plugins/twilio/models.py
+++ plugins/twilio/models.py
@@ -5,7 +5,7 @@ from contact.models import DeliveryAttempt
 class TwilioStatus(models.Model):
     attempt = models.ForeignKey(DeliveryAttempt, unique=True)
     sent_to = models.CharField(max_length=80)  # Phone number
-    sent_from = models.CharField(max_length=80)  # Phone number36)
+    sent_from = models.CharField(max_length=80)  # Phone number
     sent = models.BooleanField()
 
     objects = models.Manager()
