c000b994d63f0cff9a6ae682c896cf54f78d1101
Paul Tagliamonte <tag@pault.ag>
2014-02-04 15:04:50 -0500

    patch up view

--- plugins/twilio_voice/views.py
+++ plugins/twilio_voice/views.py
@@ -1,18 +1,20 @@
 from .models import TwilioVoiceStatus
 from django.shortcuts import render
-from ..utils import body_template_to_string,
+from ..utils import body_template_to_string
 
 
 def call(request, contact_id):
-    da = TwilioVoiceStatus.objects.get(id=contact_id)
-    template = da.attempt.template
+    status = TwilioVoiceStatus.objects.get(id=contact_id)
+    attempt = status.attempt
+    template = attempt.template
 
     return render(
         request,
         'plugins/{template}/voice.xml'.format(template=template),
         {
-            "attempt": da,
-            "body": body_template_to_string(attempt.template, 'voice', da)
+            "attempt": attempt,
+            "status": status,
+            "body": body_template_to_string(attempt.template, 'voice', attempt)
         },
         content_type="application/xhtml+xml"
     )
