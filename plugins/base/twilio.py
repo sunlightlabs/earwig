8ddf19ebdb65f511957d29056b7ae73e47f60ea1
Paul Tagliamonte <tag@pault.ag>
2014-02-06 16:03:38 -0500

    (For real this time)

--- plugins/base/twilio.py
+++ plugins/base/twilio.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def validate(fn):
-    def _(request):
+    def _(request, *args, **kwargs):
         twilio_settings = settings.CONTACT_PLUGIN_TWILIO
         # Right. We're going to validate that the incoming twilio sid is
         # the one we have on file. This will help to avoid blatent
@@ -12,5 +12,5 @@ def validate(fn):
         incoming_sid = request.POST['AccountSid']
         if incoming_sid != twilio_settings['account_sid']:
             return HttpResponse("Something went wrong.")
-        return fn(request)
+        return fn(request, *args, **kwargs)
     return _
