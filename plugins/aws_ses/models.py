920b8d176d5be2ee8cf77d0dc50dcdca38ecdfef
twneale <twneale@gmail.com>
2014-02-06 22:56:14 -0500

    Renamed aws_ses to ses in a fit of OCD

--- plugins/aws_ses/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class SESDeliveryMeta(models.Model):
-    attempt = models.ForeignKey('contact.models.DeliveryAttempt', unique=True)
-    request_id = models.CharField(max_length=36)
-    message_id = models.CharField(max_length=36)
