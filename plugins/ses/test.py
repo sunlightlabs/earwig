93c909469bb0ed08c63c43728b2d1179367de5b7
twneale <twneale@gmail.com>
2014-02-20 11:49:36 -0500

    Add sanity checks to SES tests

--- plugins/ses/test.py
+++ plugins/ses/test.py
@@ -28,121 +28,57 @@ from contact.utils import utcnow
 from ..base.tests import BaseTests
 
 
-class EmailTestCase(TestCase):
-    '''Base test case that switches the TEMPLATE_DIRS settings and
-    adds test objects to the database.
-    '''
-    def setUp(self):
-        self.create_attempt()
-
-    def create_attempt(self):
-        '''Create a test attempt to use in the tests.
-        '''
-        app = Application.objects.create(
-            name="Testyapp", contact="example@example.com",
-            template_set="cow")
-
-        person = Person.objects.create(
-            ocd_id='test', title='Mr.',
-            name='Paul Tagliamonte', photo_url="")
-
-        contact = ContactDetail.objects.create(
-            person=person, type='email',
-            value='paultag@sunlightfoundation.com', note='Holla at me', blacklisted=False)
-
-        sender = Sender.objects.create(
-            name="Testy McZample", id=uuid.uuid4(),
-            email_expires_at=utcnow() + dt.timedelta(weeks=500))
-
-        message = Message.objects.create(
-            type='fnord', sender=sender, application=app,
-            subject="Hello, World", message="HELLO WORLD")
-
-        message_recipient = MessageRecipient(
-            message=message,
-            recipient=person,
-            status='pending')
-        message_recipient.save()
-
-        attempt = DeliveryAttempt.objects.create(
-            contact=contact, status="scheduled",
-            engine="default")
-
-        attempt.messages.add(message_recipient)
-
-        attempt.save()
-        return attempt
-
-    def tearDown(self):
-        '''Restore the template dirs and delete the test objects.
-        '''
-        Application.objects.all().delete()
-        Person.objects.all().delete()
-        ContactDetail.objects.all().delete()
-        Sender.objects.all().delete()
-        Message.objects.all().delete()
-        MessageRecipient.objects.all().delete()
-        DeliveryAttempt.objects.all().delete()
-
-
-class SESMessageTest(EmailTestCase):
+class SESMessageTest(BaseTests, TestCase):
     '''Tests sending a message using a mock pystmark library. Assumes the
     library does what it's supposed to and the message is successfully sent.
     '''
+    plugin = SESContact()
     def test_message(self):
-        plugin = SESContact()
+
         attempt = DeliveryAttempt.objects.get(pk=1)
-        debug_info = plugin.send_message(attempt, debug=True)
-
-        ctx = dict(
-            attempt=attempt,
-            login_url=getattr(settings, 'LOGIN_URL', 'PUT REAL LOGIN URL HERE'))
-
-        path = 'plugins/default/email/body.html'
-        body_html = plugin.render_template(path, **ctx)
+        debug_info = self.plugin.send_message(attempt, debug=True)
+        login_url = getattr(settings, 'LOGIN_URL', 'PUT REAL LOGIN URL HERE')
+        ctx = dict(attempt=attempt, login_url=login_url)
 
         path = 'plugins/default/email/body.txt'
-        body_txt = plugin.render_template(path, **ctx)
+        body_txt = self.plugin.render_template(path, **ctx)
 
         path = 'plugins/default/email/subject.txt'
-        subject = plugin.render_template(path, **ctx)
+        subject = self.plugin.render_template(path, **ctx)
 
-        # self.assertEqual(debug_info['html'], body_html)
+        # Assert that templates got rendered to the expected output.
         self.assertEqual(debug_info['text'], body_txt)
 
+        # --------------------------------------------------------------------
+        # Now several sanity checks to make sure the model content
+        # is ending up the rendered output:
+        # --------------------------------------------------------------------
 
-class ContactDetailTest(EmailTestCase):
+        # 1) Make sure the first 300 characters of each message
+        #    are being displayed.
+        for message in attempt.messages.values_list('message__message', flat=True):
+            self.assertIn(message[:300], body_txt)
 
-    def test_blacklist(self):
-        '''Verify the plugin raises an error if it the contact
-        detail is blacklisted.
-        '''
-        email = "paultag@sunlightfoundation.com"
-        paul = ContactDetail.objects.get(value=email)
-        paul.blacklisted = True
-        paul.save()
-        plugin = SESContact()
-        attempt = DeliveryAttempt.objects.get(pk=1)
-        with self.assertRaises(ValueError):
-            plugin.send_message(attempt, debug=True)
+        # 2) Make sure the recipient's full name is displayed.
+        self.assertIn(attempt.contact.person.name, body_txt)
 
-    def test_wrong_medium(self):
-        '''Verify that the email plugin balks if told to send an sms.
-        '''
-        email = "paultag@sunlightfoundation.com"
-        paul = ContactDetail.objects.get(value=email)
-        paul.type = 'sms'
-        paul.save()
-        plugin = SESContact()
-        attempt = DeliveryAttempt.objects.get(pk=1)
-        with self.assertRaises(ValueError):
-            plugin.send_message(attempt, debug=True)
+        # 3) Make sure each sender's name is displayed.
+        for name in attempt.messages.values_list('message__sender__name', flat=True):
+            self.assertIn(name, body_txt)
+
+        # 4) Verify that the unsubscribe url is displayed.
+        self.assertIn(attempt.unsubscribe_url, body_txt)
+
+        # 5) Verify that login url is displayed.
+        self.assertIn(login_url, body_txt)
 
 
-class BounceHandlingTest(EmailTestCase):
+class BounceHandlingTest(BaseTests, TestCase):
     '''Verify that a hypothetical bounce notifaction from postmark results
     in an accurate RecieverFeedback record.
     '''
+    plugin = SESContact()
+
     def test_hard_bounce(self):
         '''Verify that hard bounces result in DeliveryAttempt.status
         being toggled to 'bad-data'.
