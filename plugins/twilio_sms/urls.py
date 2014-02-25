b755be9303f5db3b50d72d9eebea6747e2eb05f3
Paul Tagliamonte <tag@pault.ag>
2014-02-25 13:54:26 -0500

    style fix

--- plugins/twilio_sms/urls.py
+++ plugins/twilio_sms/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 
 
-urlpatterns = patterns('plugins.twilio_sms.views',
+urlpatterns = patterns(
+    'plugins.twilio_sms.views',
     url(r'^text/$', 'text', name='text'),
 )
