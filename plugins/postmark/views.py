1cae8e144ede26c131b6feea94e94e7b3ac1d9c8
twneale <twneale@gmail.com>
2014-03-05 15:54:51 -0500

    Added inbound unsolicited mail handler that sends a default email.

--- plugins/postmark/views.py
+++ plugins/postmark/views.py
@@ -3,9 +3,13 @@ import time
 import email
 import datetime as dt
 
+from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.timezone import utc
+from django.template.loader import render_to_string
+
+import pystmark
 
 from contact.models import MessageReply
 from plugins.postmark.earwig import PostmarkContact
@@ -80,10 +84,43 @@ def handle_inbound(request):
     created_at = dt.datetime.fromtimestamp(time.mktime(created_at))
     created_at = created_at.replace(tzinfo=utc)
 
-    reply = MessageReply.objects.create(
-        body=mail['TextBody'],
-        subject=mail['Subject'],
-        email=mail['FromFull']['Email'],
-        message_recip_id=int(mail['MailboxHash']),
-        created_at=created_at)
+    # Handle reply emails.
+    if mail.get('MailboxHash'):
+        reply = MessageReply.objects.create(
+            body=mail['TextBody'],
+            subject=mail['Subject'],
+            email=mail['FromFull']['Email'],
+            message_recip_id=int(mail['MailboxHash']),
+            created_at=created_at)
+        return HttpResponse()
+
+    # Handle random, uninitiated emails.
+    return handle_inbound_unsolicited(mail)
+
+
+def handle_inbound_unsolicited(mail):
+    '''Mail that arrives at this view was sent directly to cow@earwig.com
+    without an id (like cow+12345@earwig.com). In response, we'll send a
+    generic "Hi! What are you doing?" email.
+    '''
+    # Create the reply-to address.
+    if settings.POSTMARK_MX_FORWARDING_ENABLED:
+        inbound_host = settings.EARWIG_INBOUND_EMAIL_HOST
+    else:
+        inbound_host = settings.POSTMARK_INBOUND_HOST
+    reply_to_tmpl = '{0.POSTMARK_INBOUND_HASH}@{1}'
+    reply_to = reply_to_tmpl.format(settings, inbound_host)
+
+    subject_tmpl = 'plugins/default/email/unsolicited/subject.txt'
+    body_text_tmpl = 'plugins/default/email/unsolicited/body.txt'
+    body_html_tmpl = 'plugins/default/email/unsolicited/body.html'
+
+    message = pystmark.Message(
+        sender=settings.EARWIG_EMAIL_SENDER,
+        reply_to=reply_to,
+        to=mail['FromFull']['Email'],
+        subject=render_to_string(subject_tmpl, {}),
+        text=render_to_string(body_text_tmpl, {}),
+        html=render_to_string(body_html_tmpl, {}))
+    pystmark.send(message, settings.POSTMARK_API_KEY)
     return HttpResponse()
