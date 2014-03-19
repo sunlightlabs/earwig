98059ff926ae3462417015959e2511599d458faf
twneale <twneale@gmail.com>
2014-03-19 16:32:34 -0400

    Complain if recieving an inbound message reply from non-allowed email address

--- plugins/postmark/views.py
+++ plugins/postmark/views.py
@@ -12,7 +12,7 @@ from django.core.exceptions import PermissionDenied
 
 import pystmark
 
-from contact.models import MessageReply
+from contact.models import MessageReply, ContactType, Sender, MessageRecipient
 from plugins.postmark.earwig import PostmarkPlugin
 from plugins.postmark.models import (
     PostmarkDeliveryMeta,
@@ -100,6 +100,29 @@ def handle_inbound(request):
 
     mail = json.loads(request.read().decode('utf-8'))
 
+    # Retrieve the original message recip record.
+    mailbox_hash = mail.get('MailboxHash')
+    if mailbox_hash is None:
+        # Handle random, uninitiated emails.
+        return handle_inbound_unsolicited(mail)
+    message_recip_id = int(mailbox_hash)
+    message_recip = MessageRecipient.objects.get(id=message_recip_id)
+
+    # Create a list of emails allowed to participate in this thread.
+    allowed_emails = [message_recip.message.sender.email]
+    allowed_emails += message_recip.recipient.contacts.filter(
+        type=ContactType.email).values_list('value', flat=True)
+
+    # Send "what are you doing?" email if the reply isn't from the original
+    # sender or the orig recipient.
+    from_email = mail['FromFull']['Email']
+    if from_email not in allowed_emails:
+        return handle_inbound_stranger_reply(mail)
+
+    # Determine whether sender is original sender or orig recip.
+    from_original_sender = message_recip.message.sender.email == from_email
+
+    # Determine the created_at date.
     created_at = email.utils.parsedate(mail['Date'])
     created_at = dt.datetime.fromtimestamp(time.mktime(created_at))
     created_at = created_at.replace(tzinfo=utc)
@@ -109,13 +132,21 @@ def handle_inbound(request):
         reply = MessageReply.objects.create(
             body=mail['TextBody'],
             subject=mail['Subject'],
-            email=mail['FromFull']['Email'],
-            message_recip_id=int(mail['MailboxHash']),
+            from_original_sender=from_original_sender,
+            message_recip_id=message_recip_id,
             created_at=created_at)
         return HttpResponse()
 
-    # Handle random, uninitiated emails.
-    return handle_inbound_unsolicited(mail)
+
+def get_reply_to_address():
+    '''Create the reply-to address.'''
+    if settings.POSTMARK_MX_FORWARDING_ENABLED:
+        inbound_host = settings.EARWIG_INBOUND_EMAIL_HOST
+    else:
+        inbound_host = settings.POSTMARK_INBOUND_HOST
+    reply_to_tmpl = '{0.POSTMARK_INBOUND_HASH}@{1}'
+    reply_to = reply_to_tmpl.format(settings, inbound_host)
+    return reply_to
 
 
 def handle_inbound_unsolicited(mail):
@@ -123,17 +154,32 @@ def handle_inbound_unsolicited(mail):
     without an id (like cow+12345@earwig.com). In response, we'll send a
     generic "Hi! What are you doing?" email.
     '''
-    # Create the reply-to address.
-    if settings.POSTMARK_MX_FORWARDING_ENABLED:
-        inbound_host = settings.EARWIG_INBOUND_EMAIL_HOST
-    else:
-        inbound_host = settings.POSTMARK_INBOUND_HOST
-    reply_to_tmpl = '{0.POSTMARK_INBOUND_HASH}@{1}'
-    reply_to = reply_to_tmpl.format(settings, inbound_host)
-
+    reply_to = get_reply_to_address()
     subject_tmpl = 'plugins/default/email/unsolicited/subject.txt'
     body_text_tmpl = 'plugins/default/email/unsolicited/body.txt'
     body_html_tmpl = 'plugins/default/email/unsolicited/body.html'
+
+    message = pystmark.Message(
+        sender=settings.EARWIG_EMAIL_SENDER,
+        reply_to=reply_to,
+        to=mail['FromFull']['Email'],
+        subject=render_to_string(subject_tmpl, {}),
+        text=render_to_string(body_text_tmpl, {}),
+        html=render_to_string(body_html_tmpl, {}))
+    pystmark.send(message, settings.POSTMARK_API_KEY)
+    return HttpResponse()
+
+
+def handle_inbound_stranger_reply(mail):
+    '''Mail that arrives at this view was contained a valid MessageRecipient
+    id as the MailboxHash, but wasn't from the original sender or any of the
+    email accounts on file for the original recipient. This view sends
+    a reply telling the person we couldn't deliver the message.
+    '''
+    reply_to = get_reply_to_address()
+    subject_tmpl = 'plugins/default/email/stranger_reply/subject.txt'
+    body_text_tmpl = 'plugins/default/email/stranger_reply/body.txt'
+    body_html_tmpl = 'plugins/default/email/stranger_reply/body.html'
 
     message = pystmark.Message(
         sender=settings.EARWIG_EMAIL_SENDER,
